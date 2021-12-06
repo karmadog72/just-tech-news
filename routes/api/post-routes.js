b176551be6810c97f70e9e87068a26ad7b0ddef3
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-12-05 17:57:47 -0700

    vote push

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
+const sequelize = require("../../config/connection");
 
 // get all users
 router.get("/", (req, res) => {
