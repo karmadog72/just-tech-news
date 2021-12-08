90811fd515f78067351ab313d2fa4a3f8b365d91
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-12-07 17:30:31 -0700

    added comment models

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
 const sequelize = require("../../config/connection");
+const { Post, User, Comment, Vote } = require("../../models");
 
 // get all users
 router.get("/", (req, res) => {
+  console.log("======================");
   Post.findAll({
     attributes: [
       "id",
@@ -16,6 +17,21 @@ router.get("/", (req, res) => {
         ),
         "vote_count",
       ],
+    ],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
     ],
   })
     .then((dbPostData) => res.json(dbPostData))
@@ -44,6 +60,14 @@ router.get("/:id", (req, res) => {
     ],
     include: [
       {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
         model: User,
         attributes: ["username"],
       },
@@ -61,6 +85,7 @@ router.get("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
 router.post("/", (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
@@ -75,14 +100,13 @@ router.post("/", (req, res) => {
     });
 });
 
-// PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then((updatedPostData) => res.json(updatedPostData))
+  Post.upvote(req.body, { Vote, Comment, User })
+    .then((updatedVoteData) => res.json(updatedVoteData))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 });
 
