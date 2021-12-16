e9e651d2597feb0f1072bbae15acfe3c7d37a6bb
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-12-16 16:15:08 -0700

    added sessions

---
+++ controllers/home-routes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  console.log(req.session);
+  res.render("homepage", {
+    id: 1,
+    post_url: "https://handlebarsjs.com/guide/",
+    title: "Handlebars Docs",
+    created_at: new Date(),
+    vote_count: 10,
+    comments: [{}, {}],
+    user: {
+      username: "test_user",
+    },
+  });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
